00ee50e71efdffb54a9cf46a6203e02501f03087
jd1100 <github@smellslikebacon.online>
2021-10-14 13:39:35 -0400

    try svelte

--- http.go
+++ http.go
@@ -61,6 +61,11 @@ type TranscodeVideoResponse struct {
 	} `json:"body"`
 }
 
+type ApiResponse struct {
+	UploadURL string `json:"uploadUrl"`
+	UploadID  string `json:"uploadId"`
+}
+
 /*
 func getPresignedUploadUrl() string {
 	api_key := "srvacc_5c68pp78d8gimwikrm2vhzqgd"
@@ -100,6 +105,7 @@ var videoId string
 var transcodeVideoResponse TranscodeVideoResponse
 var newVideo Video
 var uploadedVideos []Video
+var apiResponse ApiResponse
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	//query := r.FormValue("searchQuery")
@@ -330,7 +336,231 @@ func listVideosHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Content-Type", "application/json")
 
+	api_key := *apiID
+	api_secret := *apiSecret
+	data := url.Values{}
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", "https://api.thetavideoapi.com/upload", strings.NewReader(data.Encode()))
+	presignedUrlresponse := PreSignedURLResponse{}
+	req.Header.Add("x-tva-sa-id", api_key)
+	req.Header.Add("x-tva-sa-secret", api_secret)
+
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	err = json.Unmarshal(body, &presignedUrlresponse)
+	//err = json.Unmarshal(body, &presignedUrlresponse)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(presignedUrlresponse)
+	for index, upload := range presignedUrlresponse.Body.Uploads {
+		fmt.Println(index)
+		if len(presignedUrlresponse.Body.Uploads) == 1 {
+			fmt.Println(upload.PresignedURL, upload.ID)
+			uploadUrl = upload.PresignedURL
+			uploadId = upload.ID
+			break
+		}
+	}
+
+	fmt.Println("lesgetit")
+
+	if r.Method == "GET" {
+		fmt.Println("get request")
+	} else {
+
+		//apiResponse.UploadURL = uploadUrl
+		//apiResponse.UploadID = uploadId
+		apiResponse := ApiResponse{
+			UploadURL: uploadUrl,
+			UploadID:  uploadId,
+		}
+		json.NewEncoder(w).Encode(apiResponse)
+
+	}
+}
+
+func apiHandler2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	maxSize := int64(1048576000)
+
+	if r.Method != "POST" {
+		fmt.Println("invalid request")
+	}
+
+	fmt.Println("new file uploaded")
+	r.ParseMultipartForm(maxSize)
+}
+
+func apiUploadFile(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "*")
+
+	if r.Method != "POST" {
+		fmt.Println("invalid request")
+	}
+
+	maxSize := int64(1048576000)
+	r.ParseMultipartForm(maxSize)
+
+	//decoder := json.NewDecoder(r.Body)
+	file, _, err := r.FormFile("videoUpload")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	api_key := *apiID
+	api_secret := *apiSecret
+	data := url.Values{}
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", "https://api.thetavideoapi.com/upload", strings.NewReader(data.Encode()))
+	presignedUrlresponse := PreSignedURLResponse{}
+	req.Header.Add("x-tva-sa-id", api_key)
+	req.Header.Add("x-tva-sa-secret", api_secret)
+
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	err = json.Unmarshal(body, &presignedUrlresponse)
+	//err = json.Unmarshal(body, &presignedUrlresponse)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(presignedUrlresponse)
+	for index, upload := range presignedUrlresponse.Body.Uploads {
+		fmt.Println(index)
+		if len(presignedUrlresponse.Body.Uploads) == 1 {
+			fmt.Println("number of uploads is 1")
+			fmt.Println(upload.PresignedURL, upload.ID)
+			uploadUrl = upload.PresignedURL
+			uploadId = upload.ID
+		}
+	}
+
+	fmt.Println("do more stuff")
+	// upload video to presigned url
+	data = url.Values{}
+	client = &http.Client{}
+	req, _ = http.NewRequest("PUT", uploadUrl, file)
+
+	req.Header.Add("Content-Type", "application/octet-stream")
+
+	res, _ = client.Do(req)
+	defer res.Body.Close()
+	//fmt.Println(res)
+
+	// transcode video using an upload
+	data = url.Values{}
+	client = &http.Client{}
+	transcodeVideoBody := &TranscodeVideoRequestBody{
+		SourceUploadID: uploadId,
+		PlaybackPolicy: "public",
+	}
+
+	b, err := json.Marshal(transcodeVideoBody)
+	u := bytes.NewReader(b)
+	transcodeVideoResponse := TranscodeVideoResponse{}
+	req, _ = http.NewRequest("POST", "https://api.thetavideoapi.com/video", u)
+
+	req.Header.Add("x-tva-sa-id", api_key)
+	req.Header.Add("x-tva-sa-secret", api_secret)
+	req.Header.Add("Content-Type", "application/json")
+
+	res, _ = client.Do(req)
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(&transcodeVideoResponse)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(transcodeVideoResponse.Body.Videos)
+	// check progress of video transcode
+	for _, videos := range transcodeVideoResponse.Body.Videos {
+		//fmt.Println(len(transcodeVideoResponse.Body.Videos))
+		fmt.Println("transcoding video...")
+		if len(transcodeVideoResponse.Body.Videos) == 1 {
+			videoId = videos.ID
+			progress = videos.Progress
+			state = videos.State
+			playbackURI = videos.PlaybackURI
+			fmt.Println(progress, state, playbackURI, videoId)
+		}
+	}
+
+	time.Sleep(5 * time.Second)
+
+	fmt.Println("monitoring video transcoding status")
+	client = &http.Client{}
+	req, _ = http.NewRequest("GET", "https://api.thetavideoapi.com/video/"+videoId, nil)
+
+	req.Header.Add("x-tva-sa-id", api_key)
+	req.Header.Add("x-tva-sa-secret", api_secret)
+
+	res, _ = client.Do(req)
+	defer res.Body.Close()
+
+	body, err = ioutil.ReadAll(res.Body)
+	err = json.Unmarshal(body, &transcodeVideoResponse)
+
+	fmt.Println("getting video transcode status")
+	for _, videos := range transcodeVideoResponse.Body.Videos {
+		progress = videos.Progress
+		playbackURI = videos.PlaybackURI
+		state = videos.State
+		break
+	}
+
+	for state != "success" || state != "failed" {
+
+		client := &http.Client{}
+		req, _ := http.NewRequest("GET", "https://api.thetavideoapi.com/video/"+videoId, nil)
+
+		req.Header.Add("x-tva-sa-id", api_key)
+		req.Header.Add("x-tva-sa-secret", api_secret)
+
+		res, _ := client.Do(req)
+
+		body, err = ioutil.ReadAll(res.Body)
+		err = json.Unmarshal(body, &transcodeVideoResponse)
+
+		for _, videos := range transcodeVideoResponse.Body.Videos {
+			newVideo = videos
+			progress = videos.Progress
+			playbackURI = videos.PlaybackURI
+			state = videos.State
+			fmt.Println(progress)
+			break
+		}
+		if state == "success" {
+			fmt.Println("video successfully transcoded")
+			fmt.Println("video playback url: " + playbackURI)
+
+			//add newVideo to uploadedVideos slice
+			uploadedVideos = append(uploadedVideos, newVideo)
+		}
+		time.Sleep(5 * time.Second)
+
+	}
+
+	//fmt.Println(body)
+
+}
 func main() {
 	//http.Handle("/", http.FileServer(http.Dir("./src/")))
 	apiID = flag.String("api-id", "hello", "startup")
@@ -345,6 +575,9 @@ func main() {
 	http.HandleFunc("/upload", videoUploadHandler)
 	http.HandleFunc("/playVideo", playVideoHandler)
 	http.HandleFunc("/videos", listVideosHandler)
+	http.HandleFunc("/api", apiHandler)
+	http.HandleFunc("/api2", apiHandler2)
+	http.HandleFunc("/apiUploadFile", apiUploadFile)
 
 	//watch video
 	http.ListenAndServe(":8001", nil)
--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,114 @@
 <script lang="ts">
+	//inputElement = document.querySelector('input[type="file"]')
+	//let pond = FilePond.create( inputElement )
+	const formData = new FormData()
 	let pond;
-	let name = "filepond"
+	let name = "videoUpload"
+	import ProgressBar from '@okrad/svelte-progressbar';
 	import FilePond, { registerPlugin } from 'svelte-filepond';
 	import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
 	import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 
-	function handleAddFile() {
-		console.log("file added")
+	registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
+	async function handleAddFile(err, fileItem) {
+		formData.append("videoUpload", fileItem)
+		let apiResponse = await fetch("http://localhost:8001/apiUploadFile", {
+			method: "POST",
+			headers: {
+				"Content-Type": "video/mp4"
+			},
+			body: formData
+		});
+		
+		/*
+		let apiResult = await apiResponse.json();
+		console.log(apiResult)
+		var uploadId = apiResult.uploadId
+		var uploadUrl = apiResult.uploadUrl
+
+		console.log(uploadId, uploadUrl)
+		console.log("file added", fileItem)
+		
+		// upload video to preassigned url
+		let uploadResponse = await fetch(uploadUrl, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/Octet-Stream"
+			},
+			body: fileItem
+			
+		});
+		
+		// null response
+		let uploadResult = await uploadResponse.json();
+		console.log(uploadResult)
+		
+		var transcodeBody = {
+			SourceUploadID: uploadId,
+			PlaybackPolicy: "public"
+		}
+
+		let transcodeResponse = await fetch("https://api.thetavideoapi.com/video", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(transcodeBody)
+		});
+		let transcodeResult = await transcodeResponse.json();
+		console.log(transcodeResult)
+
+		var video
+		var videoId
+		var state
+		var progress
+		var playbackUri
+		var uploadProgress
+		for (video in transcodeResult.Body.Videos){
+			console.log(video)
+			videoId = video.ID
+			progress = video.Progress
+			state = video.State
+			playbackUri = video.PlaybackURI
+			//fmt.Println(len(transcodeVideoResponse.Body.Videos))
+		}
+		uploadProgress = progress
+		console.log(videoId, progress, state, playbackUri)
+		*/
+
+		// check upload progress
+		/*let transcodeResponse = await fetch("https://api.thetavideoapi.com/video/" + videoId, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(transcodeBody)
+		});
+		let transcodeResult = await transcodeResponse.json();
+		console.log(transcodeResult)
+		*/
+		
+
 	}
-	function handleInit() {
-		console.log("started plugin")
+	async function handleInit() {
+		//
+		console.log("wuernkvvkenrv")
+		//getApiInfo()
 	}
+	
 </script>
 
 <main>
 	<div class="app">
 
-		<FilePond bind:this={pond} {name}
-			server="/api"
-			allowMultiple={true}
-			oninit={handleInit}
-			onaddfile={handleAddFile}/>
+	<FilePond bind:this={pond} {name}
+		server="http://localhost:8001/api2"
+		allowMultiple={false}
+		oninit={handleInit}
+		onaddfile={handleAddFile}
+	/>
+
+	<!--<ProgressBar series={uploadProgress}/>-->
 		
 	</div>
 </main>
